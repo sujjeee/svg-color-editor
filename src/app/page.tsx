41f74b2b4af7947c9f4d7eb4d66bce073464c9bb
Suraj <surajgupta.college@gmail.com>
2023-09-17 18:10:49 +0530

    test 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,124 @@
-import Image from 'next/image'
+"use client"
+import React, { useState } from 'react'
+import xmlParser from 'xml-js';
+import { Card } from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export default function page() {
+    const [pathColors, setPathColors] = React.useState({});
+    const [svgObject, setSvgObject] = useState(null);
+    const [paths, setPaths] = useState([]);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const [showEditor, setShowEditor] = useState(false);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    React.useEffect(() => {
+        if (svgObject) {
+            // @ts-ignore
+            const gElements = svgObject.svg.g;
+            // @ts-ignore
+            let updatedPaths = [];
+
+            if (gElements && Array.isArray(gElements.path)) {
+                // @ts-ignore
+                updatedPaths = [...updatedPaths, ...gElements.path];
+                // @ts-ignore
+            } else if (Array.isArray(svgObject.svg.path)) {
+                // @ts-ignore
+                updatedPaths = [...updatedPaths, ...svgObject.svg.path];
+                // @ts-ignore
+            } else if (svgObject.svg.path) {
+                // @ts-ignore
+                updatedPaths.push(svgObject.svg.path);
+            }
+
+            // @ts-ignore
+            setPaths(updatedPaths);
+        }
+    }, [svgObject]);
+
+    const handleFileChange = (e: any) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (event: any) => {
+                const svgContent = event.target.result;
+                const parsedSvgObject = xmlParser.xml2js(svgContent, { compact: true });
+
+                // @ts-ignore
+                setSvgObject(parsedSvgObject);
+                setShowEditor(true);
+                console.log("obj", svgObject)
+                console.log("paths", paths)
+            };
+            reader.readAsText(file);
+        }
+    };
+
+
+    // @ts-ignore
+    const handleColorChange = (pathId, color) => {
+        setPathColors({ ...pathColors, [pathId]: color });
+    };
+
+    const dynamicSvgString = `
+        <svg
+            viewBox="0 0 853 765"
+            className=''
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            ${paths
+            // @ts-ignore
+            .map((path) => {
+                // @ts-ignore
+                const fill = pathColors[path._attributes.id] || path._attributes.fill;
+                // @ts-ignore
+                return `<path id="${path._attributes.id}" d="${path._attributes.d}" fill="${fill}" />`;
+            })
+            .join('')}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </svg>
+    `;
+    return (
+        <main className='flex justify-center items-center h-screen'>
+            {!showEditor ? (
+                <div>
+                    <input type="file" accept=".svg" onChange={handleFileChange} />
+                </div>
+            ) : (
+                <Card className='grid grid-cols-2 gap-2 max-w-[700px] w-full max-h-[400px] h-full'>
+                    <div className=' rounded-l-md w-full h-full flex justify-center items-center p-6 '>
+                        <div className='w-full h-full justify-center flex items-center rounded-md border border-gray-300'>
+                            <svg dangerouslySetInnerHTML={{ __html: dynamicSvgString }} />
+                        </div>
+                    </div>
+                    <div className='bg-greeb-500 rounded-r-md h-full w-full flex justify-center items-center '>
+                        <div className='w-full h-full justify-center flex items-center rounded-r-md p-6 flex-col gap-4'>
+                            {/* @ts-ignore  */}
+                            {paths.map((path, index) => {
+                                return (
+                                    <div key={index} className="grid w-full max-w-sm items-center gap-1.5">
+                                        {/*  @ts-ignore */}
+                                        <Label htmlFor={path._attributes.id}>{path._attributes.id}</Label>
+                                        <Input
+                                            // @ts-ignore
+                                            id={path._attributes.id}
+                                            type="color"
+                                            //  @ts-ignore 
+                                            value={pathColors[path._attributes.id] || path._attributes.fill}
+                                            // @ts-ignore
+                                            onChange={(e) => handleColorChange(path._attributes.id, e.target.value)}
+                                            className='p-0'
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </Card>
+            )}
+        </main>
+    )
 }
+
